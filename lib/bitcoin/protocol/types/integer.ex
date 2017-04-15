65df7c388ff81893f2c1435fca6f7817a4149fb6
comboy <kacper.ciesla@gmail.com>
2017-04-15 23:39:32 +0200

    cleanup Types.Integer module

--- lib/bitcoin/protocol/types/integer.ex
+++ lib/bitcoin/protocol/types/integer.ex
@@ -1,36 +1,18 @@
 defmodule Bitcoin.Protocol.Types.Integer do
 
-  def parse(<<0xFD, data :: unsigned-little-integer-size(16)>>) do
+  @moduledoc """
+    var_int type from the Bitcoin protocol ( https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer )
+  """
+
+  def parse(binary) do
+    [data, <<>>] = parse_stream(binary)
     data
   end
 
-  def parse(<<0xFE, data :: unsigned-little-integer-size(32)>>) do
-    data
-  end
-
-  def parse(<<0xFF, data :: unsigned-native-integer-size(64)>>) do
-    data
-  end
-
-  def parse(<<data :: unsigned-integer-size(8)>>) do
-    data
-  end
-
-  def parse_stream(<<0xFD, data :: unsigned-little-integer-size(16), remaining :: binary>>) do
-    [data, remaining]
-  end
-
-  def parse_stream(<<0xFE, data :: unsigned-little-integer-size(32), remaining :: binary>>) do
-    [data, remaining]
-  end
-
-  def parse_stream(<<0xFF, data :: unsigned-native-integer-size(64), remaining :: binary>>) do
-    [data, remaining]
-  end
-
-  def parse_stream(<<data :: unsigned-integer-size(8), remaining :: binary>>) do
-    [data, remaining]
-  end
+  def parse_stream(<<0xFD, data :: unsigned-little-integer-size(16), remaining :: binary>>), do: [data, remaining]
+  def parse_stream(<<0xFE, data :: unsigned-little-integer-size(32), remaining :: binary>>), do: [data, remaining]
+  def parse_stream(<<0xFF, data :: unsigned-native-integer-size(64), remaining :: binary>>), do: [data, remaining]
+  def parse_stream(<<data :: unsigned-integer-size(8), remaining :: binary>>),               do: [data, remaining]
 
   def serialize(int) when is_integer(int) and int  < 0xFD,        do: << int :: unsigned-little-integer-size(8) >>
   def serialize(int) when is_integer(int) and int <= 0xFFFF,      do: << 0xFD, int :: unsigned-little-integer-size(16) >>
