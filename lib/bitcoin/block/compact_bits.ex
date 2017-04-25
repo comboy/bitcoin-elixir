19d87723959b34f9e09006b1227d3af8eb9c248c
comboy <kacper.ciesla@gmail.com>
2017-04-25 15:07:09 +0200

    Block.CompactBits cosmetic changes
    should also be faster without converting to list,
    but that seems irrelevant (it's rarely called)

--- lib/bitcoin/block/compact_bits.ex
+++ lib/bitcoin/block/compact_bits.ex
@@ -7,35 +7,34 @@ defmodule Bitcoin.Block.CompactBits do
     http://bitcoin.stackexchange.com/questions/2924/how-to-calculate-new-bits-value
   """
 
+  import Binary
+
   @doc """
   Encode target integer to compact bits integer
   """
   @spec encode(integer) :: integer
-  def encode(int), do: int |> Integer.digits(256) |> encode_digits
+  def encode(int), do: int |> Binary.from_integer |> encode_digits
 
   @doc """
   Docede compact bits integer to target integer
   """
   @spec decode(integer) :: integer
   def decode(int) do
-    [num | digits] = int |> Integer.digits(256)
+    {<<num>>, digits} = int |> Binary.from_integer |> split_at(1)
     digits
-      |> :binary.list_to_bin
-      |> Binary.trim_trailing
-      |> Binary.pad_trailing(num)
-      |> :binary.bin_to_list
-      |> Integer.undigits(256)
+    |> trim_trailing
+    |> pad_trailing(num)
+    |> to_integer
   end
 
 
-  defp encode_digits([first | _] = digits) when first > 0x7f, do: [0 | digits] |> encode_digits
+  defp encode_digits(<< first, _ :: binary >> = digits) when first > 0x7f, do: digits |> prepend(0) |> encode_digits
   defp encode_digits(digits) do
-    [length(digits) | digits]
-      |> Enum.take(4)
-      |> :binary.list_to_bin
-      |> Binary.pad_trailing(4)
-      |> :binary.bin_to_list
-      |> Integer.undigits(256)
+    digits
+    |> prepend(digits |> byte_size)
+    |> take(4)
+    |> pad_trailing(4)
+    |> to_integer
   end
 
 end
